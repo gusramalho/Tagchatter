ccca0b3daa329a32bb8a6658921aa733c6db8f40
GusRamalho <31655649+GustavoTheRamalho@users.noreply.github.com>
2018-10-21 01:35:53 -0300

    marca msg como parrot

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@
             var date = new Date(messages[i].created_at);
             var stringTime = [date.getHours(), date.getMinutes()].join(':');
             
-            html += "<li ";
+            html += "<li id='li"+ messages[i].id +"' ";
 
             if (messages[i].has_parrot)
               html += "class='parrotMsg'";
@@ -37,13 +37,18 @@
             html += "><div class='container'>" + 
                     "<img src='" + messages[i].author.avatar + "' class='avatar'>" +
                     "<p>" + messages[i].author.name + " <span class='time'> " + stringTime + " </span>" + 
-                    "<img class='parrotImg' value=" + messages[i].id + " src='images/light-parrot.svg'></p>" + 
+                    "<img class='parrotImg' id='"+ messages[i].id +"' src='images/light-parrot.svg'></p>" + 
                     "<p>" + messages[i].content + "</p>" +
                     "</div></li>";
           }
 
           list.innerHTML = '';
           list.innerHTML = html;
+
+          var parrotsImgs = document.querySelectorAll('.parrotImg');
+          for(var i=0; i < parrotsImgs.length; i++)
+             parrotsImgs[i].addEventListener("click", event => parrotMessage(event.target.id));
+
         });
     },3000);
 
@@ -52,12 +57,21 @@
   function parrotMessage(messageId) {
     // Faz um request para marcar a mensagem como parrot no servidor
     // Altera a mensagem na lista para que ela apareça como parrot na interface
+    return fetch(apiUrl + "/messages/" + messageId + "/parrot", {method: 'PUT'})
+      .then(() => document.querySelector("#li"+messageId).style.backgroundColor = '#efe3d7')
+      .catch(() => alert(""));
   }
 
   function sendMessage(message, authorId) {
     // Manda a mensagem para a API quando o usuário envia a mensagem
     // Caso o request falhe exibe uma mensagem para o usuário utilizando Window.alert ou outro componente visual
     // Se o request for bem sucedido, atualiza o conteúdo da lista de mensagens
+    return fetch(apiUrl + "/messages", { 
+      method: 'POST', 
+      stable: JSON.stringify(true),
+      body: JSON.stringify({"message": message, "author_id": authorId})
+     })
+     .catch((err) => console.err("Erro ao enviar mensagem: "+ err));
   }
 
   function getMe() {
@@ -67,8 +81,7 @@
       .then(response => response.json())
       .then(user => {
         document.querySelector('#userImage').src = user.avatar;
-
-        //window.sessionStorage.setItem("me", user);
+        window.sessionStorage.setItem("userId", user.id);
       });
   }
 
@@ -77,14 +90,9 @@
     fetchParrotsCount();
     getMe();
     listMessages();
+    sendMessage("aaewrwe DSA FSDa", window.sessionStorage.getItem("userId"));
   }
-
-  function uptadeList(){
-
-  }
-
 
   initialize();
 })("https://tagchatter.herokuapp.com");
-
 
